246dff863936719951d68f3a01fe445a6a732854
Inma91 <inma0810@gmail.com>
2026-02-23 11:57:26 +0100

    Update Hola.java

--- src/com/dam/Hola.java
+++ src/com/dam/Hola.java
@@ -5,6 +5,8 @@ public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola a tod@s");
+		System.out.println("Cambio en remoto");
+		
 	}
 
 }
